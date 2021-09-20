b0969a3f27958a4e14aba96c5ee7bda9c56f59ef
Dave May <echo.dave@protonmail.com>
2021-09-19 22:02:36 -0400

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	},
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		// 'plugin:react/jsx-runtime'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'2 spaces'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
